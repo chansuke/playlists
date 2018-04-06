d79975988542b9b6f0114dc22ba0c395a6124f13
chansuke <chansuke@georepublic.de>
2018-04-06 21:47:38 +0900

    Add store

---
+++ src/store.ts
@@ -0,0 +1,8 @@
+import { applyMiddleware, createStore } from 'redux';
+
+import reducers from './reducers';
+
+export default function configureStore(middlewares: any[]) {
+  const creatStoreWithMiddleware = applyMiddleware(...middlewares)(createStore);
+  return creatStoreWithMiddleware(reducers);
+}
