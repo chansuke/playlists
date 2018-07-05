90752fb6f867034c33e2848315768260210ef559
chansuke <chansuke@georepublic.de>
2018-07-05 23:02:47 +0900

    Reset

---
+++ README.md
@@ -0,0 +1,7 @@
+## Commands
+* `yarn watch`: strat dev server(localhost:8080)
+* `yarn storybook`: start storybook(localhost:6006)
+* `yarn test`: run jest
+* `yarn lint`: run tslint
+* `yarn deploy`: deploy to netlify
+
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const HtmlPlugin = require("html-webpack-plugin")
+const mode = process.env.NODE_ENV || "development"
+module.exports = {
+  mode,
+  entry: [__dirname + "/src/index.tsx"],
+  output: {
+    path: __dirname + "/public",
+    filename: "bundle.js"
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: [/node_modules/],
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.tsx?$/,
+        use: {
+          loader: "awesome-typescript-loader",
+          options: {
+            useBabel: true,
+            babelCore: "babel-core"
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlPlugin({
+      template: "src/index.html"
+    })
+  ]
+}
